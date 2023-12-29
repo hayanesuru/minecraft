d4ef181e7e698b3245f4d0857596fde4a35299f5
chena <sample41@yeah.net>
2023-12-29 03:10:40 -0600

    remove packet decoder and encoder

--- src/net.rs
+++ src/net.rs
@@ -1,10 +1,5 @@
 mod channel;
-mod decoder;
-mod encoder;
-
 pub mod c2s;
 pub mod s2c;
 
 pub use self::channel::{channel, Receiver, Sender};
-pub use self::decoder::PacketDecoder;
-pub use self::encoder::PacketEncoder;
