b22bb19444765303998fea64d358fe131b3a5cec
na3k <mc@jvavav.com>
2024-01-07 01:03:11 -0600

    update parse hex

--- ser/src/hex.rs
+++ ser/src/hex.rs
@@ -1,29 +1,30 @@
-macro_rules! parse_unsigned_hex {
-    ($t:ty) => {
+macro_rules! parse_impl {
+    ($($t:ident => $to:ident),* $(,)?) => {
+        $(
         impl Integer for $t {
             fn parse(buf: &[u8]) -> (Self, usize) {
                 let mut x = buf;
 
                 let first = match x.first() {
                     Some(x) => *x,
-                    None => return (0, 0),
+                    None => return ($t::default(), 0),
                 };
                 if first == b'+' {
                     unsafe {
                         x = x.get_unchecked(1..);
                     }
                 }
-                let mut out: $t = 0;
+                let mut out: $to = 0;
                 match x.split_first() {
                     Some((&dig, y)) => {
                         if let Some(dig) = hex_to_u8(dig) {
                             x = y;
                             out = out.wrapping_mul(16).wrapping_add(dig as _);
                         } else {
-                            return (0, 0);
+                            return ($t::default(), 0);
                         }
                     }
-                    _ => return (0, 0),
+                    _ => return ($t::default(), 0),
                 }
                 while let Some((&dig, y)) = x.split_first() {
                     if let Some(dig) = hex_to_u8(dig) {
@@ -33,9 +34,10 @@ macro_rules! parse_unsigned_hex {
                         break;
                     }
                 }
-                (out, buf.len() - x.len())
+                ($t::from_ne_bytes(out.to_ne_bytes()), buf.len() - x.len())
             }
         }
+        )*
     };
 }
 
@@ -47,11 +49,17 @@ pub fn parse_hex<T: Integer>(n: &[u8]) -> (T, usize) {
     T::parse(n)
 }
 
-parse_unsigned_hex!(u8);
-parse_unsigned_hex!(u16);
-parse_unsigned_hex!(u32);
-parse_unsigned_hex!(u64);
-
+parse_impl! {
+    u8 => u8,
+    u16 => u16,
+    u32 => u32,
+    u64 => u64,
+    i8 => u8,
+    i16 => u16,
+    i32 => u32,
+    f32 => u32,
+    f64 => u64,
+}
 const HEX_DIG: &[u8; 16] = b"0123456789abcdef";
 
 #[inline]
