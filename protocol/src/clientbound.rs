f2084b9f1628cefb1a962d54729f7872a1a8ca84
hayanesuru <hayanesuru@outlook.jp>
2026-02-13 13:51:00 +0900

    macro

--- protocol/src/clientbound.rs
+++ protocol/src/clientbound.rs
@@ -1,4 +1,4 @@
-use minecraft_data::{clientbound__configuration, clientbound__login, clientbound__status};
+use minecraft_data::{clientbound__login, clientbound__status};
 
 pub mod common;
 pub mod configuration;
@@ -8,42 +8,46 @@ pub mod ping;
 pub mod status;
 
 macro_rules! packets {
-    ($m:ty,$($rest:tt)*) => {
-        packets!(@munch $m; $($rest)*);
-    };
-    (@munch $m:ty;
-      $variant:ident = $type:ty,
-        $($tail:tt)*
-    ) => {
+    ($m:ty, $handler:ident, $($variant:ident = $type:ty),+ $(,)*) => {
+        $(
         #[automatically_derived]
         impl crate::types::Id for $type {
             type T = $m;
             const ID: $m = <$m>::$variant;
         }
-        packets!(@munch $m; $($tail)*);
-    };
-    (@munch $m:ty;
-     $variant:ident = $type:ty
-    ) => {
-        #[automatically_derived]
-        impl crate::types::Id for $type {
-            type T = $m;
-            const ID: $m = <$m>::$variant;
+        )+
+
+        pub trait $handler {
+            fn handle(&mut self, mut packet: &[u8]) -> Result<(), mser::Error> {
+                match <$m as mser::Read>::read(&mut packet)? {
+                    $(
+                        <$m>::$variant => {
+                            let e = <$type as mser::Read>::read(&mut packet)?;
+                            if !packet.is_empty() {
+                                mser::cold_path();
+                                return Err(mser::Error);
+                            }
+                            self.$variant(e);
+                        }
+                    )+
+                }
+                Ok(())
+            }
+        $(
+            fn $variant(&mut self, packet: $type);
+        )+
         }
     };
-    (@munch $m:ty; , $($tail:tt)*) => {
-        packets!(@munch $m; $($tail)*);
-    };
-    (@munch $m:ty; ,) => {};
-    (@munch $m:ty;) => {};
 }
 packets! {
     clientbound__status,
+    StatusHandler,
     status_response = status::StatusResponse<'_>,
     pong_response = ping::PongResponse,
 }
 packets! {
     clientbound__login,
+    LoginHandler,
     login_disconnect = login::LoginDisconnect<'_>,
     hello = login::Hello<'_>,
     login_finished = login::LoginFinished<'_>,
@@ -51,26 +55,27 @@ packets! {
     custom_query = login::CustomQuery<'_>,
     cookie_request = cookie::LoginCookieRequest<'_>,
 }
-packets! {
-    clientbound__configuration,
-    cookie_request = cookie::ConfigCookieRequest<'_>,
-    custom_payload = common::CustomPayload<'_>,
-    disconnect = common::Disconnect,
-    finish_configuration = configuration::FinishConfiguration,
-    keep_alive = common::KeepAlive,
-    ping = common::Ping,
-    reset_chat = common::ResetChat,
-    registry_data = configuration::RegistryData<'_>,
-    resource_pack_pop = common::ResourcePackPop,
-    resource_pack_push = common::ResourcePackPush<'_>,
-    store_cookie = common::StoreCookie<'_>,
-    transfer = common::Transfer<'_>,
-    update_enabled_features = configuration::UpdateEnabledFeatures<'_>,
-    // update_tags,
-    // select_known_packs,
-    // custom_report_details,
-    // server_links,
-    // clear_dialog,
-    // show_dialog,
-    // code_of_conduct,
-}
+// packets! {
+//     clientbound__configuration,
+//     ConfigurationHandler,
+//     cookie_request = cookie::ConfigCookieRequest<'_>,
+//     custom_payload = common::CustomPayload<'_>,
+//     disconnect = common::Disconnect,
+//     finish_configuration = configuration::FinishConfiguration,
+//     keep_alive = common::KeepAlive,
+//     ping = common::Ping,
+//     reset_chat = common::ResetChat,
+//     registry_data = configuration::RegistryData<'_>,
+//     resource_pack_pop = common::ResourcePackPop,
+//     resource_pack_push = common::ResourcePackPush<'_>,
+//     store_cookie = common::StoreCookie<'_>,
+//     transfer = common::Transfer<'_>,
+//     update_enabled_features = configuration::UpdateEnabledFeatures<'_>,
+//     update_tags,
+//     select_known_packs,
+//     custom_report_details,
+//     server_links,
+//     clear_dialog,
+//     show_dialog,
+//     code_of_conduct,
+// }
--- protocol/src/serverbound.rs
+++ protocol/src/serverbound.rs
@@ -9,53 +9,57 @@ pub mod ping;
 pub mod status;
 
 macro_rules! packets {
-    ($m:ty,$($rest:tt)*) => {
-        packets!(@munch $m; $($rest)*);
-    };
-    (@munch $m:ty;
-        $variant:ident = $type:ty,
-        $($tail:tt)*
-    ) => {
+    ($m:ty, $handler:ident, $($variant:ident = $type:ty),+ $(,)*) => {
+        $(
         #[automatically_derived]
         impl crate::types::Id for $type {
             type T = $m;
             const ID: $m = <$m>::$variant;
         }
-        packets!(@munch $m; $($tail)*);
-    };
-    (@munch $m:ty;
-        $variant:ident = $type:ty
-    ) => {
-        #[automatically_derived]
-        impl crate::types::Id for $type {
-            type T = $m;
-            const ID: $m = <$m>::$variant;
+        )+
+
+        pub trait $handler {
+            fn handle(&mut self, mut packet: &[u8]) -> Result<(), mser::Error> {
+                match <$m as mser::Read>::read(&mut packet)? {
+                    $(
+                        <$m>::$variant => {
+                            let e = <$type as mser::Read>::read(&mut packet)?;
+                            if !packet.is_empty() {
+                                mser::cold_path();
+                                return Err(mser::Error);
+                            }
+                            self.$variant(e);
+                        }
+                    )+
+                }
+                Ok(())
+            }
+        $(
+            fn $variant(&mut self, packet: $type);
+        )+
         }
     };
-    (@munch $m:ty; , $($tail:tt)*) => {
-        packets!(@munch $m; $($tail)*);
-    };
-    (@munch $m:ty; ,) => {};
-    (@munch $m:ty;) => {};
 }
 packets! {
     serverbound__handshake,
+    HandshakeHandler,
     intention = handshake::Intention<'_>,
 }
 packets! {
     serverbound__status,
+    StatusHandler,
     status_request = status::StatusRequest,
     ping_request = ping::PingRequest,
 }
 packets! {
     serverbound__login,
+    LoginHandler,
     hello = login::Hello<'_>,
     key = login::Key<'_>,
     custom_query_answer = login::CustomQueryAnswer<'_>,
     login_acknowledged = login::LoginAcknowledged,
     cookie_response = cookie::CookieResponse<'_>,
 }
-packets! {
-    serverbound__configuration,
-
-}
+// packets! {
+//     serverbound__configuration,
+// }
