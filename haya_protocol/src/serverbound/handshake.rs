9db2f6a366aafbe9292c70c71f4d129cb7e6f9a0
hayanesuru <hayanesuru@outlook.jp>
2026-02-19 14:30:29 +0900

    bungee cord handshake

--- haya_protocol/src/serverbound/handshake.rs
+++ haya_protocol/src/serverbound/handshake.rs
@@ -3,7 +3,7 @@ use crate::{ClientIntent, Utf8};
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Intention<'a> {
     pub protocol_version: Utf8<'a>,
-    pub host_name: Utf8<'a, 255>,
+    pub host_name: Utf8<'a>, // bungeecord
     pub port: u16,
     pub intention: ClientIntent,
 }
