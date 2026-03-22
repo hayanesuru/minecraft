b9637c3d8c3d2041ddbf730e72bae861bec6322a
hayanesuru <hayanesuru@outlook.jp>
2026-03-22 13:47:15 +0900

    move ConsumeEffect

--- haya_protocol/src/item.rs
+++ haya_protocol/src/item.rs
@@ -1,3 +1,4 @@
+pub mod consume_effect;
 pub mod item_attribute_modifiers;
 pub mod item_enchantments;
 pub mod kinetic_weapon;
@@ -7,6 +8,7 @@ pub mod tool;
 use crate::advancement::BlockPredicate;
 use crate::effect::MobEffect;
 use crate::food::FoodProperties;
+use crate::item::consume_effect::ConsumeEffect;
 use crate::item::item_attribute_modifiers::ItemAttributeModifiers;
 use crate::item::item_enchantments::ItemEnchantments;
 use crate::item::kinetic_weapon::KineticWeapon;
@@ -22,10 +24,7 @@ use alloc::vec::Vec;
 use haya_collection::List;
 use haya_ident::{Ident, ResourceKey, TagKey};
 use haya_nbt::Tag;
-use minecraft_data::{
-    block_entity_type, consume_effect_type, data_component_type, entity_type, item, mob_effect,
-    potion,
-};
+use minecraft_data::{block_entity_type, data_component_type, entity_type, item, potion};
 use mser::{Either, Error, Read, Reader, Utf8, V21, V32, Write, Writer};
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -270,94 +269,6 @@ pub struct Repairable<'a> {
 #[derive(Clone, Serialize, Deserialize)]
 pub struct DeathProtection<'a> {
     pub death_effects: List<'a, ConsumeEffect<'a>>,
-}
-
-#[derive(Clone)]
-pub enum ConsumeEffect<'a> {
-    Apply {
-        effects: List<'a, MobEffect<'a>>,
-        probability: f32,
-    },
-    Remove {
-        effects: HolderSet<'a, mob_effect>,
-    },
-    Clear,
-    TeleportRandomly {
-        diameter: f32,
-    },
-    PlaySound {
-        sound: Holder<SoundEvent<'a>, SoundEventRef>,
-    },
-}
-
-impl<'a> ConsumeEffect<'a> {
-    pub const fn id(&self) -> consume_effect_type {
-        match self {
-            ConsumeEffect::Apply { .. } => consume_effect_type::apply_effects,
-            ConsumeEffect::Remove { .. } => consume_effect_type::remove_effects,
-            ConsumeEffect::Clear => consume_effect_type::clear_all_effects,
-            ConsumeEffect::TeleportRandomly { .. } => consume_effect_type::teleport_randomly,
-            ConsumeEffect::PlaySound { .. } => consume_effect_type::play_sound,
-        }
-    }
-}
-
-impl<'a> Read<'a> for ConsumeEffect<'a> {
-    fn read(buf: &mut Reader<'a>) -> Result<Self, Error> {
-        Ok(match consume_effect_type::read(buf)? {
-            consume_effect_type::apply_effects => Self::Apply {
-                effects: Read::read(buf)?,
-                probability: Read::read(buf)?,
-            },
-            consume_effect_type::remove_effects => Self::Remove {
-                effects: Read::read(buf)?,
-            },
-            consume_effect_type::clear_all_effects => Self::Clear,
-            consume_effect_type::teleport_randomly => Self::TeleportRandomly {
-                diameter: Read::read(buf)?,
-            },
-            consume_effect_type::play_sound => Self::PlaySound {
-                sound: Read::read(buf)?,
-            },
-        })
-    }
-}
-
-impl<'a> Write for ConsumeEffect<'a> {
-    unsafe fn write(&self, w: &mut Writer) {
-        unsafe {
-            self.id().write(w);
-            match self {
-                Self::Apply {
-                    effects,
-                    probability,
-                } => {
-                    effects.write(w);
-                    probability.write(w);
-                }
-                Self::Remove { effects } => {
-                    effects.write(w);
-                }
-                Self::Clear => (),
-                Self::TeleportRandomly { diameter } => diameter.write(w),
-                Self::PlaySound { sound } => sound.write(w),
-            }
-        }
-    }
-
-    fn len_s(&self) -> usize {
-        self.id().len_s()
-            + match self {
-                Self::Apply {
-                    effects,
-                    probability,
-                } => effects.len_s() + probability.len_s(),
-                Self::Remove { effects } => effects.len_s(),
-                Self::Clear => 0,
-                Self::TeleportRandomly { diameter } => diameter.len_s(),
-                Self::PlaySound { sound } => sound.len_s(),
-            }
-    }
 }
 
 #[derive(Clone, Serialize, Deserialize)]
