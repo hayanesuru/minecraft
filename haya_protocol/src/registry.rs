e55bb26449b1379665d52673b13fdabd7375000b
hayanesuru <hayanesuru@outlook.jp>
2026-03-25 17:06:30 +0900

    codec PigVariant

--- haya_protocol/src/registry.rs
+++ haya_protocol/src/registry.rs
@@ -39,6 +39,9 @@ pub struct WolfVariantRef(#[mser(varint)] pub u32);
 #[derive(Clone, Copy, Serialize, Deserialize)]
 pub struct WolfSoundVariantRef(#[mser(varint)] pub u32);
 
+#[derive(Clone, Copy, Serialize, Deserialize)]
+pub struct PigVariantRef(#[mser(varint)] pub u32);
+
 impl<'a> Read<'a> for Holder<SoundEvent<'a>, SoundEventRef> {
     fn read(buf: &mut Reader<'a>) -> Result<Self, Error> {
         let id = V32::read(buf)?.0;
