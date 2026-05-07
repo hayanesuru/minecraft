c2244379b6580ca468e68eb8e8efc4fe1ee77518
hayanesuru <hayanesuru@outlook.jp>
2026-05-07 16:31:59 +0900

    fix intent packet

--- haya_protocol/src/lib.rs
+++ haya_protocol/src/lib.rs
@@ -50,37 +50,6 @@ extern crate alloc;
 #[derive(Clone, Copy, Debug)]
 pub struct Translatable<'a>(pub &'a str, pub &'a str);
 
-#[derive(Clone, Copy, Debug)]
-#[repr(u8)]
-pub enum ClientIntent {
-    Status = 1,
-    Login = 2,
-    Transfer = 3,
-}
-
-impl Write for ClientIntent {
-    unsafe fn write(&self, w: &mut Writer) {
-        unsafe {
-            w.write_byte(*self as u8);
-        }
-    }
-
-    fn len_s(&self) -> usize {
-        1
-    }
-}
-
-impl<'a> Read<'a> for ClientIntent {
-    fn read(buf: &mut Reader) -> Result<Self, Error> {
-        match V21::read(buf)?.0 {
-            1 => Ok(Self::Status),
-            2 => Ok(Self::Login),
-            3 => Ok(Self::Transfer),
-            _ => Err(Error),
-        }
-    }
-}
-
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ComponentJson<'a>(pub Utf8<'a, 262144>);
 
--- haya_protocol/src/serverbound/handshake.rs
+++ haya_protocol/src/serverbound/handshake.rs
@@ -1,9 +1,41 @@
-use crate::{ClientIntent, Utf8};
+use crate::Utf8;
+use mser::V32;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Intention<'a> {
-    pub protocol_version: Utf8<'a>,
+    pub protocol_version: V32,
     pub host_name: Utf8<'a>, // bungeecord
     pub port: u16,
     pub intention: ClientIntent,
+}
+
+#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+pub enum ClientIntent {
+    Status = 1,
+    Login = 2,
+    Transfer = 3,
+}
+
+impl mser::Write for ClientIntent {
+    unsafe fn write(&self, w: &mut mser::Writer) {
+        unsafe {
+            w.write_byte(*self as u8);
+        }
+    }
+
+    fn len_s(&self) -> usize {
+        1
+    }
+}
+
+impl<'a> mser::Read<'a> for ClientIntent {
+    fn read(buf: &mut mser::Reader) -> Result<Self, mser::Error> {
+        match mser::V21::read(buf)?.0 {
+            1 => Ok(Self::Status),
+            2 => Ok(Self::Login),
+            3 => Ok(Self::Transfer),
+            _ => Err(mser::Error),
+        }
+    }
 }
